e7ce63b35d6c8f4e9ff87c2b7fce147b56d95709
matt <matt@gmail.com>
2026-06-14 09:02:32 -0400

    agregando depedencia composer

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
+require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../controllers/template.controller.php";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
 
-error_reporting(E_ALL);
-
+// template:
 $templatep = new TemplateController();
 $templatep -> ctrtemplate();
+
+
