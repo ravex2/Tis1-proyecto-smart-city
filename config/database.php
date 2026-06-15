23b05a28766476d1c5332b5eb69a5c7050eb412c
Oscar_Gonzalez <ogonzalez@ing.ucsc.cl>
2026-06-15 19:48:36 -0400

    CRUD categoria_publicacion: crear categoria funcional

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . "/app.php";
+
+try {
+    $conexion = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8",
+        DB_USER,
+        DB_PASS
+    );
+
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
