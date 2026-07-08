3c6e7fb5c8b72cd239770418a73b960489b35cc4
Oscar_Gonzalez <ogonzalez@ing.ucsc.cl>
2026-07-08 18:51:13 -0400

    fix crear_categoria_reporte.php  id_funcionario se pasa de manera automatica y se ha integrado un select para seleccionar el area municipal al que corresponde

--- src/categorias_reporte/crear_categoria_reporte.php
+++ src/categorias_reporte/crear_categoria_reporte.php
@@ -1,13 +1,29 @@
 <?php
 require_once __DIR__ . "/../../config/database.php";
+$db = getDatabase();
+$areas = $db->query("SELECT * FROM area_municipal");
+
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+$rut = $_SESSION['user']['rut'];
+
+$consultaFuncionario = "SELECT id_funcionario FROM funcionario_municipal WHERE rut_usuario = ? ";
+$resultado = $db->query($consultaFuncionario, [$rut]);
+$funcionario = $resultado[0] ?? null;
+
+if(!$funcionario){
+    header("Location: ?ruta=dashboard");
+    exit();
+}
+
+$id_funcionario = $funcionario['id_funcionario'];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre_categoria = $_POST["nombre_categoria"];
     if (!empty($nombre_categoria)) {
-        $id_funcionario = 1;
-        $id_area_municipal = 1;
+        $id_area_municipal = $_POST["id_area_municipal"];
         
-        $db = getDatabase();
 
         $resultado = $db->execute(
             "INSERT INTO categoria_reporte (nombre_categoria, id_funcionario,id_area_municipal) VALUES(?,?,?)",
@@ -31,5 +47,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <form method="POST">
     <input type="text" name="nombre_categoria" placeholder="Nombre categoria" required>
+
+    <select name="id_area_municipal" required>
+
+        <?php foreach ($areas as $area) { ?>
+
+            <option value="<?php echo $area['id_area']; ?>">
+                <?php echo $area['nombre_area']; ?>
+            </option>
+
+        <?php } ?>
+
+    </select>
+
+
     <button type="submit">Guardar</button>
 </form>
