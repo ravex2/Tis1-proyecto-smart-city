da8adaec7e5ac705c11db0050cc7fae84b38b14d
Oscar_Gonzalez <ogonzalez@ing.ucsc.cl>
2026-07-09 11:49:40 -0400

    style , eliminando sidebar de funcionario y agregando nav de ciudadano en crear_reporte.pgp , leer_mis_reportes y ver_Reporte , todos viws de ciudadano

--- src/reporte_ciudadano/leer_mis_reportes.php
+++ src/reporte_ciudadano/leer_mis_reportes.php
@@ -4,56 +4,63 @@
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mis Reportes</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SmartCity</title>
+        <link href="https://jsdelivr.net" rel="stylesheet">
+        <link href="https://googleapis.com" rel="stylesheet">
+        <link rel="stylesheet" href="https://jsdelivr.net">
+        <link rel="stylesheet" href="/TIS1-PROYECTO-SMART-CITY/assets/css/panel.css">
+    </head>
   <body>
-  
-    <table class="table">
-    <thead>
-        <tr>
-        <th scope="col">Fecha Reporte</th>
-        <th scope="col">Estado</th>
-        <th scope="col">Descripcion</th>
-        <th scope="col">Leer Reporte</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-            $rut = $_SESSION['user']['rut'];
-            $consulta = "SELECT * FROM reporte WHERE rut_usuario='$rut'";
-            $db = getDatabase();
-            $resultado =$db->query($consulta);
+    <?php include __DIR__ . "/../../views/layout/navbar_user.php"; ?>
+    <div class="container-fluid mt-4">
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Fecha Reporte</th>
+            <th scope="col">Estado</th>
+            <th scope="col">Descripcion</th>
+            <th scope="col">Leer Reporte</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $rut = $_SESSION['user']['rut'];
+                $consulta = "SELECT * FROM reporte WHERE rut_usuario='$rut'";
+                $db = getDatabase();
+                $resultado =$db->query($consulta);
 
-            if(count($resultado) > 0){
-                foreach($resultado as $fila){
-                    $fecha =$fila['fecha'];
-                    $tipo_estado =$fila['tipo_estado'];
-                    $descripcion =$fila['descripcion'];
-                    echo "<tr>";
-                        echo "<td>".$fecha."</td>";                
-                        echo "<td>".$tipo_estado."</td>";
-                        echo "<td>".$descripcion."</td>";
-                        echo    "<td>
-                                    <a href='?ruta=ver_reporte&id_enviado=".$fila['id_reporte']."'
-                                    class='btn btn-primary rounded-pill px-4 fw-bold shadow-primary'>
-                                        Ver seguimiento
-                                    </a>
-                                </td>";
-                    echo "</tr>";
+                if(count($resultado) > 0){
+                    foreach($resultado as $fila){
+                        $fecha =$fila['fecha'];
+                        $tipo_estado =$fila['tipo_estado'];
+                        $descripcion =$fila['descripcion'];
+                        echo "<tr>";
+                            echo "<td>".$fecha."</td>";                
+                            echo "<td>".$tipo_estado."</td>";
+                            echo "<td>".$descripcion."</td>";
+                            echo    "<td>
+                                        <a href='?ruta=ver_reporte&id_enviado=".$fila['id_reporte']."'
+                                        class='btn btn-primary rounded-pill px-4 fw-bold shadow-primary'>
+                                            Ver seguimiento
+                                        </a>
+                                    </td>";
+                        echo "</tr>";
+                    }
+                }else{
+                    echo    "<tr>
+                                <td>No hay Reportes</td>
+                            </tr>";
                 }
-            }else{
-                echo    "<tr>
-                            <td>No hay Reportes</td>
-                        </tr>";
-            }
-               
-        ?>
-    </tbody>
-    </table>
+                
+            ?>
+        </tbody>
+        </table>
+    </div>
+  
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
