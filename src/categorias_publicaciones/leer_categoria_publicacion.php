a3f904a124f5db42ac901fdcf1494bc07acef786
Oscar_Gonzalez <ogonzalez@ing.ucsc.cl>
2026-07-14 16:21:06 -0400

    fix visual en leer_categoria publicacion

--- src/categorias_publicaciones/leer_categoria_publicacion.php
+++ src/categorias_publicaciones/leer_categoria_publicacion.php
@@ -25,11 +25,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de Comercios Locales</title>
+    <title>SmartCity</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/assets/css/panel.css">
+    <link rel="stylesheet" href="assets/css/panel.css">
+
+
 </head>
 <body>
 
