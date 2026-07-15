e8defc72f76d596ce771c457293c9d147901dbd7
matt <matt@gmail.com>
2026-07-14 23:48:08 -0400

    add: se agrego filtros en las tablas

--- views/pages/admin/asignacion_roles.php
+++ views/pages/admin/asignacion_roles.php
@@ -1,7 +1,5 @@
 <?php
-    ini_set('display_errors',1);
-    ini_set('display_startup_errors',1);
-    error_reporting(E_ALL);
+
     require_once __DIR__ . '/../../../models/usuario.php';
     require_once __DIR__ . '/../../../models/rol.php';
     
@@ -21,25 +19,8 @@
     $usuarioLogeado = $_SESSION['user'] ?? null;
 ?>
 
-<!doctype html>
-<html lang="es">
-    <head>
-        <title>Title</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+<? include __DIR__ . "../../../layout/header.php"; ?>
 
-        <!-- Bootstrap CSS v5.3.8 -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
-            crossorigin="anonymous"
-        />
-        <link rel="stylesheet" href="assets/css/panel.css">
-    </head>
-
-    <body>
         <div class="container-fluid">
             <div class="row">
                 <?php include __DIR__ . "/../../layout/sidebar.php"; ?>
